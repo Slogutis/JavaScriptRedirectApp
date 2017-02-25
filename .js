eff462324278aa7c88bdd36172fbf42b4446d11d
Abdullah Gabous <slogutis_dp@mohmal.com>
2017-02-26 01:50:51 +0300

    Create .js

---
+++ .js
@@ -0,0 +1,11 @@
+
+
+/* here put alert if you want like : fllow me blah bla */
+
+
+alert("here put  .............................");
+
+
+/* here your app to redirect like snapchat snapchat://add/idr_3bood 
+
+window.location.href = 'snapchat://add/idr_3bood';
